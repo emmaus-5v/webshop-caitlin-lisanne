afe6a45ba4aca244a627eeb5ac24f4739f339b3a
lisannee <lisanne.boer2004@gmail.com>
2020-12-11 09:27:09 +0000

    tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,7 +7,6 @@ CREATE TABLE products (
   price NUMERIC(10, 2)
 );
 
-<<<<<<< HEAD
 SELECT * FROM products
 WHERE name  LIKE "%Nigiri";
 
@@ -24,44 +23,7 @@ SELECT * FROM products
 WHERE name  LIKE "%Gunkan";
 
 SELECT * FROM products
-WHERE name NOT LIKE "%Nigiri";
-
-SELECT * FROM products
-WHERE name  LIKE "%salade";
-=======
-CREATE TABLE soort (
-  name VARCHAR(200)
-);
-
-CREATE TABLE nigiri_soort AS Nigiri
- SELECT * FROM products 
-  WHERE name = 'Nigiri zalm' 
-  AND name = 'Nigiri tonijn' 
-  AND name = 'Nigiri crab'; 
-
-CREATE TABLE maki_soort AS maki
- SELECT * FROM products 
-  WHERE name = 'Maki komkommer'
-  AND name = 'Maki zalm'
-  AND name = 'Maki toijn';
-
-CREATE TABLE uramaki_soort AS Uramaki 
- SELECT * FROM products
-  WHERE name = 'Uramaki california'
-  AND name = 'Uramaki Ebi furai'
-  AND name = 'Uramaki Unagi';
-
-CREATE TABLE sashimi_soort AS Sashimi 
- SELECT * FROM products 
-  WHERE name = 'Sashimi zalm'
-  AND name = 'Sashimi toijn'
-  AND name = 'Sashimi makreel';
-
-CREATE TABLE gunkan_soort AS Gunkan 
- SELECT * FROM products
- WHERE name = 'Gunkan Sake'
- AND name = 'Gunkan Maguro'
- AND name = 'Gunkan Suzuki';
+WHERE name LIKE "%salade";
 
 CREATE TABLE sidedish_soort AS Side dish
  SELECT * FROM products
@@ -71,11 +33,3 @@ CREATE TABLE sidedish_soort AS Side dish
  AND name = 'Yakitori'
  AND name = 'Ebi fry'
  AND name = 'Shrimp croquettes';
-
-CREATE TABLE salade_soort AS salade 
- SELECT * FROM products 
-  WHERE name = 'Sashimi salade'
-  AND name = 'Wakame salade'
-  AND name = 'Ebiko salade';
-
->>>>>>> d8c35b1b75895e4b208983a6ad96d0cb23ed77b2
