fa6431b5699679ae3293171dddf7a927a1093472
lisannee <lisanne.boer2004@gmail.com>
2020-12-17 14:42:31 +0000

    tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -38,15 +38,36 @@ WHERE name  LIKE "%salade";
 DROP TABLE IF EXISTS related_products;
 CREATE TABLE related_products(
   ID SERIAL PRIMARY KEY,
-  product1_id INTEGER,
-  product2_id INTEGER
+  nigiri_soort.id INTEGER,
+  sidedish_soort.id INTEGER
 );
 
 DROP TABLE IF EXISTS related_products;
 CREATE TABLE related_products(
   ID SERIAL PRIMARY KEY,
-  product1_id INTEGER,
-  product2_id INTEGER
+  maki_soort.id INTEGER,
+  salad_soort.id INTEGER
+);
+
+DROP TABLE IF EXISTS related_products;
+CREATE TABLE related_products(
+  ID SERIAL PRIMARY KEY,
+  sashimi_soort.id INTEGER,
+  salad_soort.id INTEGER
+); 
+
+DROP TABLE IF EXISTS related_products;
+CREATE TABLE related_products(
+  ID SERIAL PRIMARY KEY,
+  uramaki_soort.id INTEGER,
+  sidedish_soort.id INTEGER
+);
+
+DROP TABLE IF EXISTS related_products;
+CREATE TABLE related_products(
+  ID SERIAL PRIMARY KEY,
+  gunkan_soort.id INTEGER,
+  salad_soort.id INTEGER
 );
 
 DROP TABLE IF EXISTS nigiri_soort;
